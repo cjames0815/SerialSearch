938da215eeac37c38a0cca8f2f158d3e566ea1e4
Cameron James <cjames0815@gmail.com>
2023-09-15 10:54:00 -0400

    Serial search main

--- serialsearch.py
+++ serialsearch.py
@@ -6,7 +6,7 @@ def search (a, first: int, size: int, target: int):
         a (int): the list to search
         first (int): the list index at which the search will start
         size (int): the number of elements to search
-        target (int): the element to search for
+        target: the element to search for
 
     Returns:
         int: If target appears in the list, index of the, element
@@ -19,8 +19,10 @@ def search (a, first: int, size: int, target: int):
     found = False
 
     #while there are more elements to search 
-    #and the target hasn't been found
-    while ((i < size) and not found):
+    #and the target hasn't been found and
+    #i + first doesn't exceed the length of 
+    #the list
+    while ((i < size ) and not found and (i + first < len(a))):
         # if the current element is the target 
         if (a[i + first] == target):
             # set found to true
